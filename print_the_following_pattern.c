33edc98688ced221b21986a198d28618422817fb
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-03-24 14:34:18 +0530

    #include<stdio.h> int main() {    int n,i,j,n1;    scanf("%d",&n);    n1=n-1;    for(i=0;i<n;i++)    {      for(j=0;j<n;j++)      {        if(i==j || j==n1)       {          printf("x");        }        else        {          printf("0");        }      }      n1--;     printf(" ");
       }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-      for(j=1;j<=i;j++)
+   int n,i,j,n1;
+   scanf("%d",&n);
+   n1=n-1;
+   for(i=0;i<n;i++)
+   {
+     for(j=0;j<n;j++)
+     {
+       if(i==j || j==n1)
       {
-         printf("%d",j);
-      }
-        printf("
-");
+         printf("x");
+       }
+       else
+       {
+         printf("0");
+       }
      }
+     n1--;
+    printf("
+");
+
+   }
 }
+
