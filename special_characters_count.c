ca347adedaad9e25721ec2da4ed1cda6021bf48a
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-05-01 16:03:02 +0530

    #include<stdio.h> int main() {     char str[100];     int i,c=0,e=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {       if(str[i]>=33 && str[i]<=47 || str[i]>=91 && str[i]<=96 || str[i]>=123 && str[i]<=126||str[i]>=58 && str[i]<=64)       c++;     }
        printf("%d",c);
    }

---
+++ special_characters_count.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i,c=0,e=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+      if(str[i]>=33 && str[i]<=47 || str[i]>=91 && str[i]<=96 || str[i]>=123 && str[i]<=126||str[i]>=58 && str[i]<=64)
+      c++;
+    }
+    
+    printf("%d",c);
+}
