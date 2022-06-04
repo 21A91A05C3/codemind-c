a8c2bce82ee7f91a844ff02f32c9b7c0601b0efd
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-06-04 09:56:54 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     int i,j,arr[100][100],n,arr1[100][100],c[100][100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)        {          scanf("%d",&arr[i][j]);        }     }     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)        {          scanf("%d",&arr1[i][j]);        }     }     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)        {          c[i][j]=abs(arr[i][j]-arr1[i][j]);        }     }     for(i=0;i<n;i++)     {         for(j=0;j<n-1;j++)        {          printf("%d ",c[i][j]);        }        printf("%d",c[i][j]);        printf(" ");     }
    }

---
+++ Absolute_difference_of_two_matrices.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,j,arr[100][100],n,arr1[100][100],c[100][100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+       {
+         scanf("%d",&arr[i][j]);
+       }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+       {
+         scanf("%d",&arr1[i][j]);
+       }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+       {
+         c[i][j]=abs(arr[i][j]-arr1[i][j]);
+       }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-1;j++)
+       {
+         printf("%d ",c[i][j]);
+       }
+       printf("%d",c[i][j]);
+       printf("
+");
+    }
+    
+    
+    
+}
